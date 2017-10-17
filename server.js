799ed615216710dd632c6fd872a1569b66277467
Giuan Adauto <giuanadauto@hotmail.com>
2017-10-16 22:56:59 -0300

    doc

--- server.js
+++ server.js
@@ -4,123 +4,75 @@ const cors = require('cors');
 const app = require('express')();
 app.use(cors());
 
-board.on("ready", function() {
+board.on("ready", ()=>{//quando a placa estiver pronta, então...
   this.pinMode(7, this.MODES.OUTPUT);
   this.digitalWrite(7, 1);
 
-  const servoBase  = new five.Servo(10);
+  const servoBase  = new five.Servo(10);//entradas no arduíno, servos
   const servoHand  = new five.Servo(12);
   const leftElbow  = new five.Servo(11);
   const rightElbow = new five.Servo(13);
 
-  // Servo alternate constructor with options
-  /*
-  var servo = new five.Servo({
-    id: "MyServo",     // User defined id
-    pin: 10,           // Which pin is it attached to?
-    type: "standard",  // Default: "standard". Use "continuous" for continuous rotation servos
-    range: [0,180],    // Default: 0-180
-    fps: 100,          // Used to calculate rate of movement between positions
-    invert: false,     // Invert all specified positions
-    startAt: 90,       // Immediately move to a degree
-    center: true,      // overrides startAt if true and moves the servo to the center of the range
-  });
-  
-
-  // Add servo to REPL (optional)
-  this.repl.inject({
-    servo: servo
-  });
-  */
-
-  // Servo API
-
-  // min()
-  //
-  // set the servo to the minimum degrees
-  // defaults to 0
-  //
-  // eg. servo.min();
-
-  // max()
-  //
-  // set the servo to the maximum degrees
-  // defaults to 180
-  //
-  // eg. servo.max();
-
-  // center()
-  //
-  // centers the servo to 90°
-  //
-  // servo.center();
-
-  // to( deg )
-  //
-  // Moves the servo to position by degrees
-  //
-  // servo.to( 90 );
-
-  // step( deg )
-  //
-  // step all servos by deg
-  //
-  // eg. array.step( -20 );
-  app.get('/base/precise', (req, res)=>{//isso é muito chato
-    const position = req.query.position;
-    servoBase.to(position);
+ /*
+    as funções são acessadas fazendo requisição para o servidor, feito com javascript.
+    nodejs e uma biblioteca feita em javascript chamada johnny-five (http://johnny-five.io/) pode ser usado com várias outras
+    plataformas além do arduíno, como Intel Galileo, raspberry pi e etc
+ */
+  app.get('/base/precise', (req, res)=>{//endpoints, acessados dessa forma: localhost:3000/base/precise?position=30
+    const position = req.query.position;//<--na requisição foi passada a variável position=30, logo req.query.position = 30
+    servoBase.to(position);//mover base para posição "position", que guarda um ângulo passado na requisição
     return res.status(200).json('base moved to '+position);
   })
 
-  app.get('/base/left', (req, res)=>{
-    servoBase.max();
+  app.get('/base/left', (req, res)=>{//"endpoint" ou "rotas"
+    servoBase.max();//vai pra o grau máximo (180)
     return res.status(200).json('moved 180');
   })
 
-  app.get('/base/right', (req, res)=>{//caramba
-    servoBase.min();
+  app.get('/base/right', (req, res)=>{//"endpoint" ou "rotas"
+    servoBase.min();//vai pra o grau mínimo (0)
     return res.status(200).json('moved 0');
   })
 
-  app.get('/hand/close', (req, res)=>{
-    servoHand.to(180);
+  app.get('/hand/close', (req, res)=>{//"endpoint" ou "rotas"
+    servoHand.to(180);//poe o servo da mão pra 180 graus, fechada
     return res.status(200).json('Hand get');
   })
 
-  app.get('/hand/open', (req, res)=>{//vsf k
-    servoHand.to(70);
+  app.get('/hand/open', (req, res)=>{//"endpoint" ou "rotas"
+    servoHand.to(70);//poe o servo da mão pra 70 graus, aberta
     return res.status(200).json('Hand open');
   })
 
-  app.get('/elbow/left/up', (req, res)=>{
-    leftElbow.min();
+  app.get('/elbow/left/up', (req, res)=>{//"endpoint" ou "rotas"
+    leftElbow.min();//põe o "cotovelo" esquerdo no grau mínimo (0), 
     return res.status(200).json('Left elbow up');
   })
 
-  app.get('/elbow/left/down', (req, res)=>{
-    leftElbow.max();
+  app.get('/elbow/left/down', (req, res)=>{//"endpoint" ou "rotas"
+    leftElbow.max();//põe o "cotovelo" esquerdo no grau máximo 
     return res.status(200).json('Left elbow down');
   })
 
-  app.get('/elbow/left/precise', (req, res)=>{
+  app.get('/elbow/left/precise', (req, res)=>{//"endpoint" ou "rotas"
     const position = req.query.position;
-    leftElbow.to(position);
+    leftElbow.to(position);//põe o "cotovelo" esquerdo em um grau específico passado na requisição, igual na linha 21
     return res.status(200).json('Elbow left moved to '+position);
   })
 
-  app.get('/elbow/right/up', (req, res)=>{
-    rightElbow.min();
+  app.get('/elbow/right/up', (req, res)=>{//"endpoint" ou "rotas"
+    rightElbow.min();//põe o "cotovelo" direito no grau mínimo
     return res.status(200).json('Right elbow up');
   })
 
-  app.get('/elbow/right/down', (req, res)=>{
-    rightElbow.max();
+  app.get('/elbow/right/down', (req, res)=>{//"endpoint" ou "rotas"
+    rightElbow.max();//põe o "cotovelo" direito no grau máximo
     return res.status(200).json('Right elbow down');
   })
 
-  app.get('/elbow/right/precise', (req, res)=>{
+  app.get('/elbow/right/precise', (req, res)=>{//"endpoint" ou "rotas"
     const position = req.query.position;
-    rightElbow.to(position);
+    rightElbow.to(position);//põe o "cotovelo" direito em um grau específico passado na requisição, igual na linha 21
     return res.status(200).json('Elbow right moved to '+position);
   })
 
@@ -128,6 +80,6 @@ board.on("ready", function() {
 
 });
 
-app.listen(3000, ()=>{
+app.listen(3000, ()=>{//põe o servidor no ar
   console.log('Linstenning on port 3000');
 })
