e284144dacfae7bb2c3bee43fee654b1f32c9306
Giuan Adauto <giuanadauto@hotmail.com>
2017-11-28 19:08:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const cors = require('cors');
 const app = require('express')();
 app.use(cors());
 
+//rekognito
+
+let express = require('express')
+  
+const bodyParser= require('body-parser');
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({extended: true, limit: '50mb'}));
+
+let AWS = require('aws-sdk');
+AWS.config.loadFromPath('./aws-config.json');
+
+let rekognition = new AWS.Rekognition({apiVersion: '2016-06-27'});
+//
+
 board.on("ready", ()=>{//quando a placa estiver pronta, então...
 
   const servoBase  = new five.Servo(10);//entradas no arduíno, servos
@@ -75,6 +90,104 @@ board.on("ready", ()=>{//quando a placa estiver pronta, então...
   })
 
 
+  //rekgnito
+
+  let atob = require('atob');
+  function getBinary(encodedFile) {
+      let base64Image = encodedFile.split("data:image/jpeg;base64,")[1];
+      let binaryImg = atob(base64Image);
+      let length = binaryImg.length;
+      var ab = new ArrayBuffer(length);
+      let ua = new Uint8Array(ab);
+      for (var i = 0; i < length; i++) {
+        ua[i] = binaryImg.charCodeAt(i);
+      }
+  
+      return ab;
+  }
+  
+  CollectionId = "paradigmas-project"
+  app.post('/v1/indexface', (req, res)=>{
+      const image = req.body.image;
+      const label = req.body.label;
+      if(!image)
+          return res.status(200).json('image is required');
+      if(!label)
+          return res.status(200).json('label is required');
+  
+      let params = {
+          CollectionId: CollectionId,
+          DetectionAttributes: [
+          ], 
+          ExternalImageId: label,
+          Image: {
+              Bytes: getBinary(image)
+          }
+       };
+  
+      rekognition.indexFaces(params, function(err, data) {
+          if (err) {
+              console.log(err, err.stack);
+              return res.status(500).json(err);
+          } else
+              return res.status(200).json(data);
+              console.log(data);
+      });
+  })
+  
+  app.post('/v1/search', (req, res)=>{
+      const image = req.body.image;
+      
+      if(!image)
+          return res.status(200).json('image is required');
+  
+      let params = {
+          CollectionId: CollectionId, 
+          FaceMatchThreshold: 80,
+          Image: {
+              Bytes: getBinary(image)
+          }, 
+          MaxFaces: 1
+      };
+  
+      rekognition.searchFacesByImage(params, function(err, data) {
+          if (err) {
+              console.log(err, err.stack);
+              return res.status(500).json(err);
+          }
+          console.log(data);
+          if(data.FaceMatches.length == 0)
+              return res.status(200).json({labe:'Not found', confidence: 0});
+                  
+          const result = {
+              label: data.FaceMatches[0].Face.ExternalImageId,
+              confidence: data.FaceMatches[0].Face.Confidence
+          };
+          console.log(data.FaceMatches[0].Face);
+          return res.status(200).json(result);
+      });
+  })
+  
+  app.post('/v1/detectext', (req, res)=>{
+      const image = req.body.image;
+      
+      if(!image)
+          return res.status(200).json('image is required');
+  
+      var params = {
+          Image: {
+              Bytes: getBinary(image),
+          }
+      };
+  
+      rekognition.detectText(params, (err, data)=>{
+          if (err) {
+              console.log(err, err.stack);
+              return res.status(500).json(err);
+          }
+          return res.status(200).json(data);
+      });
+  })
 
 });
 
